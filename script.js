bdddc9211c2207e57aba57d27c85eece17e94930
Md Maruf Hasan <marufhasancse21@gmail.com>
2026-03-25 21:53:20 +0600

    New One file

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const products = [
     { id: 1, name: "Wireless Headphones", price: 99.99, image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=500" },
     { id: 2, name: "Smart Watch", price: 149.50, image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500" },
     { id: 3, name: "Premium Backpack", price: 75.00, image: "https://images.unsplash.com/photo-1553062407-98eeb64c6a62?w=500" },
+    
+    { id: 5, name: "Premium Backpack", price: 75.00, image: "https://images.unsplash.com/photo-1553062407-98eeb64c6a62?w=500" },
+    
+    { id: 6, name: "Premium Backpack", price: 75.00, image: "https://images.unsplash.com/photo-1553062407-98eeb64c6a62?w=500" },
+
     { id: 4, name: "Mechanical Keyboard", price: 120.00, image: "https://images.unsplash.com/photo-1511467687858-23d96c32e4ae?w=500" }
 ];
 
